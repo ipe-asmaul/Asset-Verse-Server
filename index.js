b9f6624d71e175ffab7ae18846d33cf9022d5c1c
Asmaul <watch.asmaulhossain@gmail.com>
2025-12-20 04:29:37 +0600

    user and asset adding functionality added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 require('dotenv').config();
 const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = process.env.MONGODB_URI
 const PORT = process.env.SERVER_PORT || 5000
 
@@ -23,10 +23,80 @@ async function run() {
     app.get('/', (req, res)=>{
         res.send('server is running ? ')
     })
+    app.post('/user-register', async(req, res)=>{
+        const DB = client.db('Asset-Verse')
+        const collection = DB.collection('users')
+        const data = req.body;
+        const isInList = await collection.findOne({email: data.email});
+        if(isInList){
+            return res.send({message: 'User already exists'})
+        }
+        const result = await collection.insertOne(data);
+        res.send(result)
+    })
+
+  app.get('/user', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('users')
+    const email = req.query.email;
+    const query = {email: email}  
+    const result = await collection.findOne(query);
+    res.send(result)
+  })
+
+  app.post('/add-asset', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('assets')
+    const data = req.body;
+    const result = await collection.insertOne(data);
+    res.send(result)
+  })
 
 
+  app.get('/my-assets', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('assets')
+    const query = {}
+    const email = req.query.email;
+    if(email){
+        query.hrEmail = email
+    }
+   
+    const result = await collection.find(query).toArray();
+    res.send(result)
+
+  })
+
+  app.post('/requests', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('requests')  
+    const data = req.body;
+    const result = await collection.insertOne(data);
+    res.send(result)
+  })
+  app.get('/requests', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('requests')
+    const query = {}
+    const email = req.query.email;
+    if(email){
+        query.hrEmail = email
+    } 
+    
+    const result = await collection.find(query).toArray();
+    res.send(result)
+
+  })
 
 
+  app.patch('/requests', async(req, res)=>{
+    const DB = client.db('Asset-Verse')
+    const collection = DB.collection('requests') 
+    const data = req.body; 
+    const id = req.query.id;
+    const result = await collection.updateOne({_id: new ObjectId(id)}, {$set: {requestStatus: data.requestStatus, approvalDate: data.approvalDate, processedBy: data.processedBy}});
+    res.send(result)
+  })
 
 
 
@@ -38,7 +108,7 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
