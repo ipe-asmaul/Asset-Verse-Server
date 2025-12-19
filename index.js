9f53c4489b8248f4ed7734a16b51d39838853452
Asmaul <watch.asmaulhossain@gmail.com>
2025-12-19 14:35:54 +0600

    Server Side Created

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require('express');
+require('dotenv').config();
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = process.env.MONGODB_URI
+const PORT = process.env.SERVER_PORT || 5000
+
+const app = express()
+app.use(cors())
+app.use(express.json())
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    app.get('/', (req, res)=>{
+        res.send('server is running ? ')
+    })
+
+
+
+
+
+
+
+
+
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+app.listen(PORT)
